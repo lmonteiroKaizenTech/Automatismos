59efc01d580748475dee8f87ae9ac234f786edca
Leandro Monteiro <leandrocmonteiro2005@gmail.com>
2024-09-06 15:44:52 +0100

    06/09-15:44

--- tests/scripts/tarefasversao.spec.ts
+++ tests/scripts/tarefasversao.spec.ts
@@ -5,6 +5,7 @@ const TEMP = 60000;
 test('Versões', async ({ page }) => {
 
     //test.setTimeout(TEMP)
+    await page.setViewportSize({ width: 1920, height: 1000 });
 
     // #region Variáveis
     let ambiente, responsavel: boolean = true;
@@ -91,17 +92,21 @@ test('Versões', async ({ page }) => {
         }
         if (versao)
         {
-            await page.getByPlaceholder('Introduza o valor aqui').first().fill(ambiente + versao);
-            await page.getByPlaceholder('Selecione uma opção').first().fill(ambiente);
+            await page.getByPlaceholder('Introduza o valor aqui').first().type(ambiente + versao);
             await page.waitForTimeout(2000);
-            const Suggestions1 = await page.locator('.ms-Suggestions-container .ms-Button').first().textContent();
-            if (!Suggestions1?.includes(ambiente)) await page.getByPlaceholder('Selecione uma opção').first().fill(ambiente); await page.waitForTimeout(1000);
-            await page.keyboard.press('Enter');
-            await page.getByPlaceholder('Selecione uma opção').fill(versao);
+            await page.getByPlaceholder('Selecione uma opção').first().type(ambiente);
+            await page.waitForTimeout(2000);
+            // const Suggestions1 = await page.locator('.ms-Suggestions-container .ms-Button').first().textContent();
+            // if (!Suggestions1?.includes(ambiente)) await page.getByPlaceholder('Selecione uma opção').first().fill(ambiente); await page.waitForTimeout(1000);
+            // await page.keyboard.press('Enter');
+            await page.locator('.ms-Suggestions-container .ms-Button').first().click();
+            await page.waitForTimeout(2000);
+            await page.getByPlaceholder('Selecione uma opção').type(versao);
             await page.waitForTimeout(2000);
             const Suggestions2 = await page.locator('.ms-Suggestions-container .ms-Button').first().textContent();
             if (!Suggestions2?.includes(versao)) await page.getByPlaceholder('Selecione uma opção').first().fill(versao); await page.waitForTimeout(1000);
-            await page.keyboard.press('Enter');
+            // await page.keyboard.press('Enter');
+            await page.locator('.ms-Suggestions-container .ms-Button').first().click();
             await page.locator('.ReactClientForm-editButtons .ms-Button').first().click();
             await page.waitForTimeout(2000);
         }
@@ -113,6 +118,13 @@ test('Versões', async ({ page }) => {
     await page.goto('https://kaizentechsa.sharepoint.com/sites/oper/s1/Lists/Tarefas%20de%20Verso/AllItems.aspx');
     await page.waitForTimeout(2000);
 
+    await page.click(`span:text("Código Versão")`);
+    await page.waitForTimeout(3000);
+    await page.click(`span:text("Filtrar por")`);
+    await page.click(`span:text("${versao}")`);
+    await page.waitForTimeout(3000);
+    await page.click(`span:text("Aplicar")`);
+
     const titulo = await page.$$('[role="gridcell"] [role="button"]');
 
     for (var i = 0; i < titulo.length; i++)
@@ -120,6 +132,7 @@ test('Versões', async ({ page }) => {
         aux = await titulo[i].textContent();
         titulos.push(aux);
     }
+    console.log(titulos);
 
     // #endregion
 
@@ -127,17 +140,35 @@ test('Versões', async ({ page }) => {
 
     await page.goto('https://kaizentechsa.sharepoint.com/sites/oper/s1/Lists/Tarefas%20de%20Distribuio%20de%20Verses/AllItems.aspx');
     await page.waitForTimeout(2000);
-
-    await page.getByRole('menuitem').first().click();
-    await page.getByPlaceholder('Selecione uma opção').first().fill(ambiente + versao);
-    await page.waitForTimeout(2000);
-    await page.keyboard.press('Enter');
-    await page.getByPlaceholder('Selecione uma opção').nth(1).type(titulos[0]);
-    // await page.waitForSelector(selector);
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000);
-    if (responsavel) await page.getByPlaceholder('Introduza um nome ou endereço de e-mail').fill('Ines tomas');
-    else await page.getByPlaceholder('Introduza um nome ou endereço de e-mail').fill('joana carvalho');
+    await page.getByRole('menuitem').nth(1).click();
+    await page.waitForTimeout(3000);
+    await page.locator('cf-action-wrapper button').click();
+    await page.waitForTimeout(15000);
+    
+    for (var i = 0; i < titulos.length; i++)
+    {
+        // For dentro de um for
+        await page.waitForTimeout(5000);
+        await page.keyboard.press('a');
+        await page.getByPlaceholder('Escreva para filtrar').type(titulos[i]);
+        await page.keyboard.press('Enter');
+        await page.locator('.field-C_x00f3_digo_x0020_Distribui_x00-htmlGrid_1').nth(-2).click({ force: true });
+        await page.waitForTimeout(3000);
+        await page.keyboard.press('a');
+        await page.getByPlaceholder('Escreva para filtrar').type(ambiente + versao);
+        await page.keyboard.press('Enter');
+        await page.waitForTimeout(3000);
+        await page.locator('.field-Respons_x00e1_vel-htmlGrid_1').nth(-2).click({ force: true });
+        await page.waitForTimeout(3000);
+        await page.keyboard.press('a');
+        await page.waitForTimeout(2000);
+        if (responsavel) await page.fill('.ms-BasePicker-input', 'Ines Tomas');
+        else await page.fill('.ms-BasePicker-input', 'Joana');
+        await page.waitForTimeout(2000);
+        await page.keyboard.press('Enter');
+        await page.waitForTimeout(1000);
+        await page.click(`#NewRowPlaceholderID`);
+    }
 
     // #endregion
 
--- tests/scripts/teste.spec.ts
+++ tests/scripts/teste.spec.ts
@@ -5,6 +5,7 @@ const TEMP = 60000;
 test('Versões', async ({ page }) => {
 
     //test.setTimeout(TEMP)
+    await page.setViewportSize({ width: 1920, height: 1000 });
     // #region Login
     await page.goto('https://kaizentechsa.sharepoint.com/sites/oper/s1/Lists/Verses/AllItems.aspx'); 
     await page.fill('#i0116', 'lmonteiro@kaizen.tech');
@@ -15,28 +16,76 @@ test('Versões', async ({ page }) => {
     await page.waitForTimeout(2000);
     // #endregion
 
-    function teste() {
-        
-    }
-
     await page.goto('https://kaizentechsa.sharepoint.com/sites/oper/s1/Lists/Tarefas%20de%20Distribuio%20de%20Verses/AllItems.aspx');
     await page.waitForTimeout(2000);
+    await page.getByRole('menuitem').nth(1).click();
+    await page.waitForTimeout(3000);
+    await page.locator('cf-action-wrapper button').click();
 
-    await page.getByRole('menuitem').first().click();
-    await page.getByPlaceholder('Selecione uma opção').first().fill('Teste');
-    await page.waitForTimeout(2000);
-    await page.keyboard.press('Enter');
-    await page.getByPlaceholder('Selecione uma opção').nth(1).type('', { delay: 100 });
+    for (var i = 0; i < 20; i++)
+        {
+            await page.waitForTimeout(15000);
+            await page.keyboard.press('a');
+            await page.getByPlaceholder('Escreva para filtrar').type('teste');
+            await page.keyboard.press('Enter');
+            await page.locator('.field-C_x00f3_digo_x0020_Distribui_x00-htmlGrid_1').nth(-2).click({ force: true });
+            await page.waitForTimeout(3000);
+            await page.keyboard.press('a');
+            await page.getByPlaceholder('Escreva para filtrar').type('teste');
+            await page.keyboard.press('Enter');
+            await page.waitForTimeout(3000);
+            await page.locator('.field-Respons_x00e1_vel-htmlGrid_1').nth(-2).click({ force: true });
+            await page.waitForTimeout(3000);
+            await page.keyboard.press('a');
+            await page.waitForTimeout(2000);
+            await page.fill('.ms-BasePicker-input', 'Joana');
+            await page.waitForTimeout(2000);
+            await page.keyboard.press('Enter');
+            await page.waitForTimeout(1000);
+            await page.click(`#NewRowPlaceholderID`);
+        }
 
-    await page.waitForTimeout(10000);
-    // await page.waitForSelector(selector);
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000);
-    await page.getByPlaceholder('Introduza um nome ou endereço de e-mail').fill('Ines tomas');
-    await page.waitForSelector('');
+    // await page.goto('https://kaizentechsa.sharepoint.com/sites/oper/s1/Lists/Tarefas%20de%20Verso/AllItems.aspx');
+    // await page.waitForTimeout(2000);
 
-    await page.waitForTimeout(10000);
+    // await page.getByRole('button').nth(2).click();
+    // await page.waitForTimeout(10000);
 
+    // await page.goto('https://kaizentechsa.sharepoint.com/sites/oper/s1/Lists/Tarefas%20de%20Verso/AllItems.aspx');
+    // await page.waitForTimeout(2000);
+
+    // await page.click(`span:text("Código Versão")`);
+    // await page.waitForTimeout(3000);
+    // await page.click(`span:text("Filtrar por")`);
+    // await page.click(`span:text("KTCore.3.4")`);
+
+    // await page.goto('https://kaizentechsa.sharepoint.com/sites/oper/s1/Lists/Tarefas%20de%20Distribuio%20de%20Verses/AllItems.aspx');
+    // await page.waitForTimeout(2000);
+    // await page.getByRole('menuitem').nth(1).click();
+    // await page.waitForTimeout(3000);
+    // await page.locator('cf-action-wrapper button').click();
+    // await page.waitForTimeout(15000);
+    // await page.keyboard.press('a');
+    // await page.getByPlaceholder('Escreva para filtrar').type('#');
+    // await page.keyboard.press('Enter');
+    // await page.locator('.field-C_x00f3_digo_x0020_Distribui_x00-htmlGrid_1').nth(-2).click({ force: true });
+    // await page.waitForTimeout(3000);
+    // await page.keyboard.press('a');
+    // await page.getByPlaceholder('Escreva para filtrar').type('AC');
+    // await page.keyboard.press('Enter');
+    // await page.waitForTimeout(3000);
+    // await page.locator('.field-Respons_x00e1_vel-htmlGrid_1').nth(-2).click({ force: true });
+    // await page.waitForTimeout(3000);
+    // await page.keyboard.press('a');
+    // await page.waitForTimeout(2000);
+    // await page.fill('.ms-BasePicker-input','joana');
+    // await page.waitForTimeout(2000);
+    // await page.keyboard.press('Enter');
+
+    // await page.getByRole('menuitem').nth(1).click();
+    // await page.waitForTimeout(1000);
+    // await page.click('#NewRowPlaceholderID');
+    
     await page.close();
 
 });
